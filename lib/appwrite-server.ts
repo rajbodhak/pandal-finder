75f738b6c0c18128c1179cba57988acc4630774f
Raj Bodhak <rajlaptop2004@gmail.com>
2026-01-15 19:43:26 +0530

    build(env): Use NEXT_PUBLIC_ prefix for Appwrite variables

--- lib/appwrite-server.ts
+++ lib/appwrite-server.ts
@@ -2,17 +2,17 @@ import { Client, Databases, Storage, Query } from 'node-appwrite';
 
 // Server-only credentials 
 const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT!)
-    .setProject(process.env.APPWRITE_PROJECT_ID!)
-    .setKey(process.env.APPWRITE_API_KEY!);
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+    .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY!);
 
 export const serverDatabases = new Databases(client);
 export const serverStorage = new Storage(client);
 
 // Export these for use in API routes
-export const DATABASE_ID = process.env.APPWRITE_DATABASE_ID!;
-export const COLLECTION_ID = process.env.APPWRITE_PANDALS_COLLECTION_ID!;
-export const BUCKET_ID = process.env.APPWRITE_STORAGE_BUCKET_ID!;
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
+export const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_PANDALS_COLLECTION_ID!;
+export const BUCKET_ID = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ID!;
 
 // Re-export Query for convenience
 export { Query };
