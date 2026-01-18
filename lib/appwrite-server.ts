b348f86d8617034e50ca43686cdb03621d232723
Raj Bodhak <rajlaptop2004@gmail.com>
2026-01-18 15:30:25 +0530

    fix(env): use server-side Appwrite API key

--- lib/appwrite-server.ts
+++ lib/appwrite-server.ts
@@ -4,7 +4,7 @@ import { Client, Databases, Storage, Query } from 'node-appwrite';
 const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
-    .setKey(process.env.NEXT_PUBLIC_APPWRITE_API_KEY!);
+    .setKey(process.env.APPWRITE_API_KEY!);
 
 export const serverDatabases = new Databases(client);
 export const serverStorage = new Storage(client);
